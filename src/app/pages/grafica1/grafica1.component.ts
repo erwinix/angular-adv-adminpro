96b7c70628578346c1aa2cf19c9b1402b2c5d680
DeveloperJS <erwin.18.2012@gmail.com>
2021-08-22 13:49:18 -0400

    Fin sección 6

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class Grafica1Component implements OnInit {
+
+  @Input() title: string = 'Sin Titulo'
+  labels1: string[] = ['Pan', 'Refresco', 'Tacos'];
+
+  data1 = [
+    [350, 450, 100],
+  ];
 
   constructor() { }
 
